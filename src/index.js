98860a29cc6ac39f282a5f11049c9699fd5c9d2e
Sidheshwar Choure <sidheshwarchoure@gmail.com>
2023-07-27 18:46:03 +0530

    new change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <p>"I am learning React. My life is getting better."</p>,
+  document.getElementById("root")
+);
